dc6b897d8d113a46a2b4f4582632a286e9fd60a2
Vyctor <dev.vyctor@gmail.com>
2021-03-09 22:46:56 -0300

    feat: react properties

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,28 +1,17 @@
+import RepositoryItem from "./RepositoryItem";
+
+const repository = {
+  name: "Nome do repositório",
+  description: "lorem lorem ipsum lorem ipsum lorem ipsum",
+  link: "https://github.com/vyctor",
+};
+
 export default function RepositoryList() {
   return (
     <section className="repository-list">
       <h1>Lista de repositórios</h1>
       <ul>
-        <li>
-          <strong>unform</strong>
-          <p>Forms in React</p>
-          <a href="#">Acessar repositório</a>
-        </li>
-        <li>
-          <strong>unform</strong>
-          <p>Forms in React</p>
-          <a href="#">Acessar repositório</a>
-        </li>
-        <li>
-          <strong>unform</strong>
-          <p>Forms in React</p>
-          <a href="#">Acessar repositório</a>
-        </li>
-        <li>
-          <strong>unform</strong>
-          <p>Forms in React</p>
-          <a href="#">Acessar repositório</a>
-        </li>
+        <RepositoryItem repository={repository} />
       </ul>
     </section>
   );
