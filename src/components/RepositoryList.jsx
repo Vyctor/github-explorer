7a0db027eceff76c347d06caebd44833c3d7a89e
Vyctor <dev.vyctor@gmail.com>
2021-03-15 21:49:34 -0300

    feat: listing repositories

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -2,12 +2,6 @@ import { useEffect, useState } from "react";
 import RepositoryItem from "./RepositoryItem";
 import "../styles/repositories.scss";
 
-const repository = {
-  name: "Nome do repositório",
-  description: "lorem lorem ipsum lorem ipsum lorem ipsum",
-  link: "https://github.com/vyctor",
-};
-
 export default function RepositoryList() {
   const [repositories, setRepository] = useState([]);
 
@@ -21,7 +15,11 @@ export default function RepositoryList() {
     <section className="repository-list">
       <h1>Lista de repositórios</h1>
       <ul>
-        <RepositoryItem repository={repository} />
+        {repositories.map((repository) => {
+          return (
+            <RepositoryItem key={repository.name} repository={repository} />
+          );
+        })}
       </ul>
     </section>
   );
